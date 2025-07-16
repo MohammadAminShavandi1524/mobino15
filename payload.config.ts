403673d8ff6b7c4d5883930e56362733e0d7889f
MohammadAminShavandi1524 <maminshavandi@gmail.com>
2025-07-16 14:43:16 +0430

    Improve authentication logic

--- payload.config.ts
+++ payload.config.ts
@@ -22,6 +22,7 @@ export default buildConfig({
     },
   },
   collections: [Users, Media ,Categories ],
+  // cookiePrefix : "mobino",
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
