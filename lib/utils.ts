9ded2aa5af896029296071829f967d2653afc677
MohammadAminShavandi1524 <maminshavandi@gmail.com>
2025-08-06 00:57:18 +0430

    shadcn sidebar installed

--- lib/utils.ts
+++ lib/utils.ts
@@ -20,36 +20,6 @@ export function convertToPersianNumber(num: number | string): string {
   return num.toString().replace(/\d/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[parseInt(d)]);
 }
 
-export function formatToPersianGroupedNumber(input: number | string): string {
-  const persianDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
-
-  const englishNumber = input.toString().replace(/[^0-9]/g, "");
-  const grouped = Number(englishNumber).toLocaleString("en-US"); // 1234567 -> 1,234,567
-
-  return grouped.replace(/\d/g, (d) => persianDigits[+d]);
-}
-
-export const formatAsCurrency = (value: string) => {
-  const numericValue = value.replace(/[^0-9.]/g, "");
-
-  const parts = numericValue.split(".");
-  const formattedValue =
-    parts[0] + (parts.length > 1 ? parts[1]?.slice(0, 2) : "");
-
-  if (!formattedValue) return "";
-
-  const numberValue = parseFloat(formattedValue);
-
-  if (isNaN(numberValue)) return "";
-
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(numberValue);
-};
-
 export const formatWithThousandSeparator = (value: string) => {
   const numericValue = value.replace(/\D/g, ""); // فقط رقم‌ها
 
@@ -57,13 +27,6 @@ export const formatWithThousandSeparator = (value: string) => {
 
   return Number(numericValue).toLocaleString("en-US"); // فقط جداکننده هزارگان
 };
-
-function toFarsiDigitsWithGrouping(input: string): string {
-  const persianDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
-  return input
-    .replace(/\d/g, (d) => persianDigits[+d])
-    .replace(/\B(?=(\d{3})+(?!\d))/g, "٬");
-}
 
 export function capitalizeFirstLetter(str: string) {
   if (!str) return "";
